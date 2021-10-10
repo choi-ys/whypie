0eb8405e795ae321b5f891776beac4aea08e7027
choi-ys <project.log.062@gmail.com>
2021-10-11 05:26:40 +0900

    [2021-10-11][authorization-api]용석:토큰 발급/갱신/만료 발생 시, 로깅 처리

--- application/service/authorization-api/src/main/kotlin/me/whypie/service/AuthorizationService.kt
+++ application/service/authorization-api/src/main/kotlin/me/whypie/service/AuthorizationService.kt
@@ -8,14 +8,17 @@ import me.whypie.model.vo.Principal
 import me.whypie.model.vo.Token
 import me.whypie.repository.BlackListTokenCacheRepo
 import me.whypie.repository.WhiteListTokenCacheRepo
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
+import java.lang.IllegalArgumentException
 
+private val logger = KotlinLogging.logger {  }
 /**
  * @author : choi-ys
  * @date : 2021-10-08 오전 7:04
  */
 @Service
-class TokenService(
+class AuthorizationService(
     private val tokenProvider: TokenProvider,
     private val tokenVerifier: TokenVerifier,
     private val blackListTokenCacheRepo: BlackListTokenCacheRepo,
@@ -26,21 +29,27 @@ class TokenService(
         val token = tokenProvider.createToken(principal)
         addBlackList(principal.identifier)
         whiteListTokenCacheRepo.save(WhiteListTokenCache(principal.identifier, token))
+        logger.info("[ISSUE][principal :{}, {}]", principal.identifier, principal.authorities)
         return token
     }
 
     fun refresh(refreshToken: String): Token {
         val verifyResult = tokenVerifier.verify(refreshToken)
         val principal = verifyResult.principal
+        whiteListTokenCacheRepo.findById(principal.identifier).orElseThrow() {
+            throw IllegalArgumentException("")
+        }
         addBlackList(principal.identifier)
         val token = tokenProvider.createToken(principal)
         whiteListTokenCacheRepo.save(WhiteListTokenCache(principal.identifier, token))
+        logger.info("[REFRESH][principal :{}, {}]", principal.identifier, principal.authorities)
         return token
     }
 
     fun expire(accessToken: String) {
         val principal = tokenVerifier.verify(accessToken).principal
         addBlackList(principal.identifier)
+        logger.info("[EXPIRE][principal :{}, {}]", principal.identifier, principal.authorities)
     }
 
     private fun addBlackList(username: String) {
