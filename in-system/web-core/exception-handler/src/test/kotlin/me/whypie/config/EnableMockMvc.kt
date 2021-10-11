ccb3bdc17ddc97793356efa4c19b4ea55b7eaefa
choi-ys <project.log.062@gmail.com>
2021-10-12 01:36:57 +0900

    [2021-10-12][exception-handler]용석:gradle build warning 수정

--- in-system/web-core/exception-handler/src/test/kotlin/me/whypie/config/EnableMockMvc.kt
+++ in-system/web-core/exception-handler/src/test/kotlin/me/whypie/config/EnableMockMvc.kt
@@ -4,17 +4,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.web.filter.CharacterEncodingFilter
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 /**
  * @author : choi-ys
  * @date : 2021/09/02 8:41 오후
  */
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
 @AutoConfigureMockMvc
 @Import(
     EnableMockMvc.Config::class
