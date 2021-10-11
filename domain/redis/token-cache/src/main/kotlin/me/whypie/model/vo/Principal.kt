41aef15d9a931ff121cc377f23880ba0073cfdcd
choi-ys <project.log.062@gmail.com>
2021-10-12 08:42:33 +0900

    Revert "[2021-10-12][token-cache]용석:인증된 사용자를 표현하는 Principal 객체의 속성 수정"
     - This reverts commit b4aeb4fd
     - [2021-10-12][token-cache]용석:코드 원복에 따른 commit revert

--- domain/redis/token-cache/src/main/kotlin/me/whypie/model/vo/Principal.kt
+++ domain/redis/token-cache/src/main/kotlin/me/whypie/model/vo/Principal.kt
@@ -1,13 +1,16 @@
 package me.whypie.model.vo
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.User
-
 /**
  * @author : choi-ys
  * @date : 2021-10-10 오전 3:43
  */
 data class Principal(
     var identifier: String,
-    var authorities: Set<SimpleGrantedAuthority>,
-) : User(identifier, "", authorities)
+    var authorities: String,
+) {
+    companion object {
+        fun <T> mapTo(identifier: String, set: Set<T>): Principal {
+            return Principal(identifier, set.joinToString(","))
+        }
+    }
+}
