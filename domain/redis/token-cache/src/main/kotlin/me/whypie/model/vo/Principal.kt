b4aeb4fd1b3b6954a7ec3c09463950dbceee547c
choi-ys <project.log.062@gmail.com>
2021-10-12 03:36:48 +0900

    [2021-10-12][token-cache]용석:인증된 사용자를 표현하는 Principal 객체의 속성 수정
     - spring-security의 UserDetails 상속 처리에 따른 권한 속성의 자료형 변경

--- domain/redis/token-cache/src/main/kotlin/me/whypie/model/vo/Principal.kt
+++ domain/redis/token-cache/src/main/kotlin/me/whypie/model/vo/Principal.kt
@@ -1,16 +1,13 @@
 package me.whypie.model.vo
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+
 /**
  * @author : choi-ys
  * @date : 2021-10-10 오전 3:43
  */
 data class Principal(
     var identifier: String,
-    var authorities: String,
-) {
-    companion object {
-        fun <T> mapTo(identifier: String, set: Set<T>): Principal {
-            return Principal(identifier, set.joinToString(","))
-        }
-    }
-}
+    var authorities: Set<SimpleGrantedAuthority>,
+) : User(identifier, "", authorities)
