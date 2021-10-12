426313841dc0e8af4c701479c470289d16d184bb
choi-ys <project.log.062@gmail.com>
2021-10-12 09:35:02 +0900

    [2021-10-12][token-cache]용석:Principal 객체 변환 함수 추가 (Principal -> UserDetails:spring-security)

--- domain/redis/token-cache/src/main/kotlin/me/whypie/model/vo/Principal.kt
+++ domain/redis/token-cache/src/main/kotlin/me/whypie/model/vo/Principal.kt
@@ -1,5 +1,9 @@
 package me.whypie.model.vo
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import java.util.stream.Collectors
+
 /**
  * @author : choi-ys
  * @date : 2021-10-10 오전 3:43
@@ -8,6 +12,16 @@ data class Principal(
     var identifier: String,
     var authorities: String,
 ) {
+    fun convert(): User {
+        return User(
+            identifier,
+            "",
+            authorities.split(",").stream()
+                .map { SimpleGrantedAuthority(it) }
+                .collect(Collectors.toSet())
+        )
+    }
+
     companion object {
         fun <T> mapTo(identifier: String, set: Set<T>): Principal {
             return Principal(identifier, set.joinToString(","))
