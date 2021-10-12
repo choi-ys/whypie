2d2430475a698c69d26c18ed2eb1e86d701d80a3
choy-ys <project.log.062@gmail.com>
2021-10-12 15:40:11 +0900

    [2021-10-12][project-core]용석:ProjectRepository 및 TC 작성

---
+++ domain/rds/project-core/src/main/kotlin/me/whypie/repository/ProjectRepo.kt
@@ -0,0 +1,11 @@
+package me.whypie.repository
+
+import me.whypie.model.entity.Project
+import org.springframework.data.jpa.repository.JpaRepository
+
+/**
+ * @author : choi-ys
+ * @date : 2021/10/12 2:18 오후
+ */
+interface ProjectRepo : JpaRepository<Project, Long> {
+}
