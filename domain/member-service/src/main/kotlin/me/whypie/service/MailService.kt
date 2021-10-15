cbf3c2c95aa511aff99a90664d9942004a65cbbc
choy-ys <project.log.062@gmail.com>
2021-10-15 18:42:42 +0900

    [2021-10-15][member-service]용석:메일 전송 실패 예외 처리

--- domain/member-service/src/main/kotlin/me/whypie/service/MailService.kt
+++ domain/member-service/src/main/kotlin/me/whypie/service/MailService.kt
@@ -17,7 +17,12 @@ class MailService(
     // TODO: 용석(2021-10-04) : javaMailSender.send의 성공 여부와 발송 정보를(발송여부, 발송메일, 인증번호) 포함한 응답 처리
     fun sendSignupCertificationMail(member: Member): Int {
         val certificationNumber = (100000..200000).random()
-        javaMailSender.send(signupCertificationMailTemplate(member.email, certificationNumber))
+        try {
+            javaMailSender.send(signupCertificationMailTemplate(member.email, certificationNumber))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw RuntimeException("메일 발송에 실패하였습니다.")
+        }
         return certificationNumber
     }
 }
