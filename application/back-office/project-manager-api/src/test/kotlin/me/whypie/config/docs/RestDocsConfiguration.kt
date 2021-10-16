5dd7f603ab452c42e54448edbd6356d169e59ab3
choi-ys <project.log.062@gmail.com>
2021-10-17 05:10:20 +0900

    [2021-10-17][project-manager-api]용석:Spring REST Docs 설정 부 변경

---
+++ application/back-office/project-manager-api/src/test/kotlin/me/whypie/config/docs/RestDocsConfiguration.kt
@@ -0,0 +1,44 @@
+package me.whypie.config.docs
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler
+import org.springframework.restdocs.operation.preprocess.Preprocessors
+
+/**
+ * @author : choi-ys
+ * @date : 2021-10-17 오전 4:18
+ */
+@TestConfiguration
+class RestDocsConfiguration {
+
+    @Bean
+    fun restDocumentationResultHandler(): RestDocumentationResultHandler {
+        return MockMvcRestDocumentation.document(
+            "{class-name}/{method-name}",
+            Preprocessors.preprocessRequest(
+                Preprocessors.modifyUris()
+                    .scheme(HTTP)
+                    .host(DEV_HOST)
+                    .removePort(),
+                Preprocessors.prettyPrint()
+            ),
+            Preprocessors.preprocessResponse(
+                Preprocessors.modifyUris()
+                    .scheme(HTTP)
+                    .host(DEV_HOST)
+                    .removePort(),
+                Preprocessors.prettyPrint())
+        )
+    }
+
+    companion object {
+        val HTTP: String = "http"
+        val HTTPS: String = "https"
+        val BASE_HOST: String = "whypie.me"
+        val DEV_HOST: String = "dev-project-api.$BASE_HOST"
+        val STG_HOST: String = "stg-project-api.$BASE_HOST"
+        val PRD_HOST: String = "prd-project-api.$BASE_HOST"
+    }
+}
