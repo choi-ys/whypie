f84899938a37c71959076190a747a69a09d7b6d2
choi-ys <project.log.062@gmail.com>
2021-10-17 08:58:30 +0900

    [2021-10-17][entity-core]용석:Enum class description 항목 추가

--- domain/rds/entity-core/src/main/kotlin/me/whypie/domain/model/entity/project/ProjectStatus.kt
+++ domain/rds/entity-core/src/main/kotlin/me/whypie/domain/model/entity/project/ProjectStatus.kt
@@ -4,6 +4,7 @@ package me.whypie.domain.model.entity.project
  * @author : choi-ys
  * @date : 2021/10/12 2:06 오후
  */
-enum class ProjectStatus {
-    ENABLE, DISABLE
+enum class ProjectStatus(val description: String) {
+    ENABLE("사용 가능"),
+    DISABLE("사용 불가능");
 }
--- domain/rds/entity-core/src/main/kotlin/me/whypie/domain/model/entity/project/ProjectType.kt
+++ domain/rds/entity-core/src/main/kotlin/me/whypie/domain/model/entity/project/ProjectType.kt
@@ -4,7 +4,7 @@ package me.whypie.domain.model.entity.project
  * @author : choi-ys
  * @date : 2021/10/12 2:06 오후
  */
-enum class ProjectType {
-    SERVICE,
-    BACK_OFFICE
+enum class ProjectType(val description: String) {
+    SERVICE("서비스"),
+    BACK_OFFICE("백오피스");
 }
